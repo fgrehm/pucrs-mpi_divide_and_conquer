c5f1e0c6553a4d2a47d2f0e07cdb3311fc46ef9d
Fabio Rehm <fgrehm@gmail.com>
2016-05-18 00:00:20 -0300

    Reserve a part of the array to order locally while we wait for children nodes to order smaller arrays

--- divide_and_conquer.c
+++ divide_and_conquer.c
@@ -15,7 +15,7 @@
 
 #define DEBUG 1
 
-int my_rank, proc_n, tree_height, conquer_condition, parent, left_child, right_child;
+int my_rank, proc_n, tree_height, conquer_condition, parent, left_child, right_child, local_processing_percentage;
 
 void divide_and_conquer(int *arr, int arr_size);
 void conquer(int *arr, int arr_size);
@@ -24,27 +24,41 @@ void send_job(int *arr, int arr_size, int dest);
 void receive_job(int **arr, int* arr_size, int source);
 void send_results(int *arr, int arr_size, int dest);
 void receive_results(int *arr, int arr_size, int source);
+void merge_arrays(int* arr, int arr_size, int offset, int* other_arr, int other_arr_size);
 
+void debug_array(int *arr, int arr_size);
 void my_log(char *fmt, ...);
 
 int usage() {
-  printf("divide-and-conquer ARRAY_SIZE\n");
+  if (my_rank == ROOT)
+    printf("divide-and-conquer ARRAY_SIZE [LOCAL_PROCESSING_PERCENTAGE]\n");
   return 1;
 }
 
 int main(int argc, char *argv[]) {
   int arr_size;
 
-  if (argc != 2) { return usage(); }
-  if (sscanf(argv[1], "%i", &arr_size) != 1) {
-    printf("\nInvalid array size: %s\n", argv[1]);
-    return usage();
-  }
-
   MPI_Init(&argc, &argv);
 
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &proc_n);
+
+  if (argc < 2 || argc > 3) {
+    MPI_Finalize();
+    return usage();
+  }
+  if (sscanf(argv[1], "%i", &arr_size) != 1) {
+    MPI_Finalize();
+    if (my_rank == ROOT) printf("\nInvalid array size: %s\n", argv[1]);
+    return usage();
+  }
+  if (argc == 3) {
+    if (sscanf(argv[2], "%i", &local_processing_percentage) != 1) {
+      MPI_Finalize();
+      if (my_rank == ROOT) printf("\nInvalid percentage: %s\n", argv[2]);
+      return usage();
+    }
+  }
 
   tree_height = (int)log2(proc_n+1);
   conquer_condition = arr_size / pow(2, tree_height-1) + 1;
@@ -63,18 +77,11 @@ int main(int argc, char *argv[]) {
   int i;
   if (my_rank == ROOT) {
     arr = calloc(arr_size, sizeof(int));
-    // init array with worst case for sorting
     for (i = 0 ; i < arr_size; i++)
       arr[i] = arr_size-i;
 
 #ifdef DEBUG
-    printf("\nBefore: ");
-    for (i = 0; i < 10; i++)
-      printf("%05d ", arr[i]);
-    printf("... ");
-    for (i = arr_size - 11; i < arr_size; i++)
-      printf("%05d ", arr[i]);
-    printf("\n");
+    printf("\nBefore: "); debug_array(arr, arr_size);
 #endif
 
     divide_and_conquer(arr, arr_size);
@@ -84,13 +91,7 @@ int main(int argc, char *argv[]) {
 
 #ifdef DEBUG
   if (my_rank == ROOT) {
-    printf("\nAfter:  ");
-    for (i = 0; i < 10; i++)
-      printf("%05d ", arr[i]);
-    printf("... ");
-    for (i = arr_size - 11; i < arr_size; i++)
-      printf("%05d ", arr[i]);
-    printf("\n");
+    printf("\nAfter:  "); debug_array(arr, arr_size);
   }
 #endif
 
@@ -102,22 +103,33 @@ int main(int argc, char *argv[]) {
 void divide_and_conquer(int *arr, int arr_size) {
   my_log("Divide and conquer array of size %d", arr_size);
 
-  int half_size = arr_size / 2;
+  int offset = arr_size * (local_processing_percentage / 100.0);
+  int half_size = (arr_size - offset) / 2;
 
-  send_job(arr, half_size, left_child);
-  send_job(arr + half_size, half_size, right_child);
+  send_job(arr + offset, half_size, left_child);
+  send_job(arr + offset + half_size, half_size, right_child);
 
-  // TODO: Do some processing here
+  if (offset > 0) {
+    my_log("Ordering first %d elements locally", offset);
+    bubble_sort(arr, offset);
+  }
 
   my_log("Waiting for results from %d", left_child);
-  receive_results(arr, half_size, left_child);
+  receive_results(arr + offset, half_size, left_child);
   my_log("Waiting for results from %d", right_child);
-  receive_results(arr + half_size, half_size, right_child);
+  receive_results(arr + offset + half_size, half_size, right_child);
 
   my_log("Interleaving");
-  int *new_arr = interleaving(arr, arr_size);
-  my_log("Copying");
-  memcpy(arr, new_arr, arr_size * sizeof(int));
+  int *new_arr = interleaving(arr + offset, arr_size - offset);
+
+  if (offset == 0) {
+    my_log("Copying");
+    memcpy(arr, new_arr, arr_size * sizeof(int));
+    free(new_arr);
+  } else {
+    my_log("Merging arrays");
+    merge_arrays(arr, arr_size, offset, new_arr, arr_size - offset);
+  }
   my_log("DONE");
 
   if (my_rank != ROOT) {
@@ -166,6 +178,33 @@ void send_results(int *arr, int arr_size, int dest) {
 void receive_results(int *arr, int arr_size, int source) {
   MPI_Status status;
   MPI_Recv(arr, arr_size, MPI_INT, source, TAG_RESULT_PAYLOAD, MPI_COMM_WORLD, &status);
+}
+
+void merge_arrays(int* arr, int arr_size, int offset, int* other_arr, int other_arr_size) {
+  int* aux_arr = calloc(offset, sizeof(int));
+  memcpy(aux_arr, arr, offset * sizeof(int));
+
+  int i1 = 0, i2 = 0, i_arr;
+  for (i_arr = 0; i_arr < arr_size; i_arr++) {
+    if (((i1 < offset) && (aux_arr[i1] <= other_arr[i2]))
+        || (i2 == other_arr_size)) {
+      arr[i_arr] = aux_arr[i1++];
+    } else {
+      arr[i_arr] = other_arr[i2++];
+    }
+  }
+
+  free(aux_arr);
+}
+
+void debug_array(int *arr, int arr_size) {
+  int i;
+  for (i = 0; i < 11; i++)
+    printf("%07d ", arr[i]);
+  printf("... ");
+  for (i = arr_size - 12; i < arr_size; i++)
+    printf("%07d ", arr[i]);
+  printf("\n");
 }
 
 void my_log(char *fmt, ...) {
